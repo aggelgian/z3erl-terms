37d4ee149472bd4816a8d3adaa0d3aa0edc90bc4
Aggelos Giantsios <aggelgian@gmail.com>
2015-04-12 18:44:37 +0300

    1st attempt at adding bitstrings & improper lists

--- erl.py
+++ erl.py
@@ -6,17 +6,23 @@ from z3 import *
 T = Datatype('Term')
 TL = Datatype('TList')
 IL = Datatype('IList')
+BvL = Datatype('BvList')
 # Term
+T.declare('nil')
 T.declare('int', ('ival', IntSort()))
-T.declare('real', ('rval', RealSort()))
-T.declare('lst', ('lval', TL))
+T.declare('flt', ('fval', RealSort()))
+T.declare('lst', ('hd', T), ('tl', T))
 T.declare('tpl', ('tval', TL))
 T.declare('atm', ('aval', IL))
+T.declare('bit', ('bval', BvL))
 # TermList
-TL.declare('nil')
-TL.declare('cons', ('hd', T), ('tl', TL))
+TL.declare('tnil')
+TL.declare('tcons', ('thd', T), ('ttl', TL))
 # IntList
-IL.declare('anil')
-IL.declare('acons', ('ahd', IntSort()), ('atl', IL))
+IL.declare('inil')
+IL.declare('icons', ('ihd', IntSort()), ('itl', IL))
+# BitVectorList
+BvL.declare('vnil')
+BvL.declare('vcons', ('vhd', BitVecSort(1)), ('vtl', BvL))
 # Return Datatypes
-T, TL, IL = CreateDatatypes(T, TL, IL)
+T, TL, IL, BvL = CreateDatatypes(T, TL, IL, BvL)
